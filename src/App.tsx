3f88cd3bd2b9bb5f4485df43f55236c618ecdf1d
Rohit Pal <rohit.coding.0123@gmail.com>
2023-07-16 17:36:51 +0530

    Completed the project by Vaildating the expense form using form Hook and zod.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,12 @@ import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-
-export const categories = ["Groceries", "Utilities", "Entertainment"];
+import categories from "./expense-tracker/categories";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
-    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
-    { id: 4, description: "ccc", amount: 10, category: "Utilities" }
+    { id: 1, description: "Example-Milk", amount: 0, category: "Groceries" }
   ]);
 
   const visibleExpense = selectedCategory
@@ -21,7 +17,11 @@ function App() {
   return (
     <div>
       <div className="mb-5">
-        <ExpenseForm />
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
       </div>
       <div className="mb-3">
         <ExpenseFilter
